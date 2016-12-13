55b215eb240186a68ffa9fcc67397de7b32a9233
adrya407 <costean_adrian@yahoo.com>
2016-12-12 16:54:07 -0800

    Add files via upload
    Tema 2, subpunctul 1

---
+++ Tema2.1.ino
@@ -0,0 +1,29 @@
+// avr-libc library includes
+#include <avr/io.h> //  can be omitted
+#include <avr/interrupt.h> // can be omitted
+                         
+void setup()
+{
+//pinMode(LEDPIN, OUTPUT);
+DDRB = DDRB | B00100000; 
+
+// initialize Timer1
+cli();         // disable global interrupts
+TCCR1A = 0;    // set entire TCCR1A register to 0
+TCCR1B = 0;    // set entire TCCR1A register to 0
+
+// enable Timer1 overflow interrupt:
+TIMSK1 |= (1 << TOIE1);
+TCNT1=0x0BDC;
+TCCR1B |= (1 << CS12); 
+// enable global interrupts:
+sei();
+}
+
+ISR(TIMER1_OVF_vect)
+{
+//PORTB ^= ~_BV(PORTB5); //am incercat sa setam prin registrii si nu functioneaza
+TCNT1=0x0BDC; // reload the timer preload
+}
+
+void loop() {}
